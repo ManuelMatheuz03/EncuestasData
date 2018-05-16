fdfa00979a5fe2057cb70fc56c77723b83f3c01a
ManuelMatheuz03 <38057638+ManuelMatheuz03@users.noreply.github.com>
2018-05-15 22:23:49 -0500

    EncuestasDataBiding

---
+++ Encuestas/CambioColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Encuestas
+{
+    class CambioColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = Color.Green;
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
